034530ed4d71ecf33e5ef0a5183e228bff636f29
henriqueholz <henriqueholz@live.com>
2022-07-02 15:34:58 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,17 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL)
+  },
+
+  async disconnect () {
+    await this.client.close()
+  },
+
+  getCollection (Name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
