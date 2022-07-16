49025b184e61ff87f2fa4f512aa97b184680b546
henriqueholz <henriqueholz@live.com>
2022-07-15 21:01:01 -0300

    feat: add LogMongoRepository to a SignUpFactory

--- src/infra/db/mongodb/log-repository/log.ts
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -1,5 +1,5 @@
 import { LogErrorRepository } from '@/data/protocols/log-error-repository'
-import { MongoHelper } from '../mongo-helper'
+import { MongoHelper } from '../helpers/mongo-helper'
 
 export class LogMongoRepository implements LogErrorRepository {
   async logError (stack: string): Promise<void> {
